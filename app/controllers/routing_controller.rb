5baac2a9959fef41703da851962ab83f963bd50e
Aram Zadikian <brancusi@gmail.com>
2016-08-11 18:13:14 -0700

    Add instructions for new feature

--- app/controllers/routing_controller.rb
+++ app/controllers/routing_controller.rb
@@ -2,7 +2,11 @@ class RoutingController < ApplicationJsonApiResourcesController
   def optimize_route
     authorize :routing
 
-    # The magik should happen with routific
+    # 1. Prepare the request data
+    # 2. POST to routific to the min-idle endpoint: https://api.routific.com/min-idle
+    # 3. Send response to the client
+    # 4. render json: routific_response
+
     render json: {response: 'Yes!'}
   end
 end
