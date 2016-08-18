1836f712a30d1e3bb164a52cc2328607587e2c32
Aram Zadikian <brancusi@gmail.com>
2016-08-17 17:12:26 -0700

    WIP - Seed updates

--- test/factories/notifications_factory.rb
+++ test/factories/notifications_factory.rb
@@ -1,17 +1,9 @@
 FactoryGirl.define do
   factory :notification do
-    notification_state Notification.notification_states[:pending]
-
-    trait :pending do
-      notification_state { Notification.notification_states[:pending] }
-    end
+    notification_rule
 
     trait :processed do
       notification_state { Notification.notification_states[:processed] }
-    end
-
-    before(:create) do |notification, evaluator|
-      notification.notification_rule = NotificationRule.all.sample
     end
   end
 end
