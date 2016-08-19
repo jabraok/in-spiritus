3751c3f9abd1254f99f1d145d8e90174eabb49cc
Aram Zadikian <brancusi@gmail.com>
2016-08-19 13:06:22 -0700

    Alterntive approach to notification renderer

--- app/utils/mail_utils.rb
+++ app/utils/mail_utils.rb
@@ -1,31 +1,13 @@
-class MailUtils
-  include MailGunUtils
-
-  def send_email(recipients, subject, body_html=nil, body_text=nil, attachments=[])
-    validate(recipients,
-      subject,
-      body_html,
-      body_text,
-      attachments)
-
-    from = {
-      :email => ENV['SALES_FROM_EMAIL'],
-      :first => "Aram",
-      :last => "Zadikian"
-    }
-
-    super(from,
-      recipients,
-      subject,
-      body_html,
-      body_text,
-      attachments)
+module MailUtils
+  def mg_client
+		@mail_gun_client ||= Mailgun::Client.new(ENV['MAIL_GUN_API_KEY'])
   end
 
-  private
-  def validate(recipients, subject, body_html, body_text, attachments)
-		raise "recipients can not empty" if recipients.nil? || !recipients.any?
-		raise "subject can not epty" if subject.empty?
-		raise "Both of body_html and body_text can not empty" if body_html.empty? && body_text.empty?
-	end
+  def send_notification(notification)
+		p "Sending email #{notification}"
+
+    msg = notification.build_message
+
+    mg_client.send_message(ENV['MAIL_GUN_DOMAIN'], msg) if msg.present?
+  end
 end
